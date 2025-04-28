7fd1a9e189f73e92cc6bd1ee72643d9266ebc49b
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-04-28 17:28:49 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const userAge = document.getElementById("user-age").value
+  const userDay = document.getElementById("day").value
+
+  // proces
+  if ((userDay == "2" || userDay == "3") || (userAge > 12 && age < 21)) {
+    // Display the number
+    document.getElementById("answer").innerHTML = "<p>You're eligible for student pricing !</p>"
+  } else{
+    document.getElementById("answer").innerHTML = "<p>You must pay regular price !</p>"
+}
+}
