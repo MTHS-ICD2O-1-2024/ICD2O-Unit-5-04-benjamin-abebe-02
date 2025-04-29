71161d26808094a033379b390d2df85d51b39a46
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-04-29 17:30:45 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -9,13 +9,12 @@
 function myButtonClicked() {
   const userAge = document.getElementById("user-age").value
   const days = document.getElementsByName("day")
-  const tuesday = days[1].checked  // Tuesday is index 1
-  const thursday = days[3].checked // Thursday is index 3
+
 
   // process
-  if ((tuesday || thursday) || (userAge > 12 && userAge < 21)) {
-    document.getElementById("answer").innerHTML = "<p>You're eligible for student pricing!</p>"
-  } else {
-    document.getElementById("answer").innerHTML = "<p>You must pay regular price!</p>"
-  }
-}
+  if (($day == "Tuesday" || $day == "tuesday" || $day == "thursday" || $day == "Thursday") || ($age > 12 && $age < 21)) {
+        document.getElementById("answer").innerHTML = "<h4>You're eligible for student pricing!</h4>"
+        } else {
+        document.getElementById("answer").innerHTML = "<h4>You must pay regular price!</h4>"
+        }
+      }
